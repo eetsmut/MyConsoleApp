8da73d47c4f09027ccddf86cc8da2faade3098a9
eetsmut <eetsmut@gmail.com>
2022-11-22 14:59:51 +0900

    First commit

--- MyConsoleApp/ConsoleApp/Program.cs
+++ MyConsoleApp/ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace MyConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello C# World! ");
+            Console.WriteLine("Hello C# World! ");
         }
     }
 }
